f2c01ad20c734ace9f6b7b94554866d87045473f
Dmitriy-75 <dimakasimov29@gmail.com>
2024-09-06 19:30:35 +0300

    Update module_6.3.py

--- module_6.3.py
+++ module_6.3.py
@@ -1,17 +1,14 @@
-class Horse():
+class Horse:
 
     def __init__(self):
         self.sound = 'Frrr'
         self.x_distance = 0
-        # super().__init__(y_distance)
-        # super().__init__(sound)
-        super().__init__()
 
     def run(self, dx):
         self.x_distance += dx
-        return self.x_distance
 
-class Eagle():
+
+class Eagle:
 
     def __init__(self):
         self.sound = 'I train, eat, sleep, and repeat'
@@ -19,12 +16,15 @@ class Eagle():
 
     def fly(self, dy):
         self.y_distance += dy
-        return self.y_distance
+
 
 class Pegasus(Horse, Eagle):
+    def __init__(self):
+        Horse.__init__(self)
+        Eagle.__init__(self)
 
     def move(self, dx, dy):
-        return super().run(dx), super().fly(dy)
+        super().run(dx), super().fly(dy)
 
     def get_pos(self):
         return self.x_distance, self.y_distance
